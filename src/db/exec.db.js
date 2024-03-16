36381dc28f265cba6e49a696ab514ca35e0f2721
Darwin Zamora <darwinzc325@gmail.com>
2024-03-16 10:28:29 -0500

    Ejecutando la base de datos

--- src/db/exec.db.js
+++ src/db/exec.db.js
@@ -1,30 +1,30 @@
 import fs from "node:fs"
 import path from "node:path"
-import { Connection as db } from "./connection.ts"
-import { exit } from "node:process"
+import { Connection as db } from "./connect.js"
 
-const getDb = async () => {
-  const [results] = await db.query("SELECT * FROM tv_series")
-  console.log(results)
-}
-getDb()
-exit()
-fs.readFile(path.resolve("src/db") + "/db.sql", "utf-8", async (err, data) => {
-  if (err) {
-    console.log(err)
-    return
-  }
+const exec = async () => {
+  fs.readFile(
+    path.resolve("src/db") + "/db.sql",
+    "utf-8",
+    async (err, data) => {
+      if (err) {
+        console.log(err)
+        return
+      }
 
-  const sentences = data.split(";")
-
-  try {
-    for (let i = 0; i < sentences.length; i++) {
-      if (sentences[i].trim() !== "") {
-        await db.query(sentences[i])
+      const sentences = data.split(";")
+      try {
+        for (let i = 0; i < sentences.length; i++) {
+          if (sentences[i].trim() !== "") {
+            await db.query(sentences[i])
+          }
+        }
+        console.log("Database created successfully")
+      } catch (err) {
+        console.log(err)
       }
     }
-    console.log("Database created successfully")
-  } catch (err) {
-    console.log(err)
-  }
-})
+  )
+}
+
+exec()
